80699dcb42837f0676d3f405dcbfc0691362d735
Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>
2022-01-23 22:13:57 +0330

    add login api

--- go.mod
+++ go.mod
@@ -3,14 +3,16 @@ module github.com/gomonitoring/http-server
 go 1.17
 
 require (
-	github.com/andybalholm/brotli v1.0.2 // indirect
+	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible // indirect
 	github.com/go-ozzo/ozzo-validation/v4 v4.3.0 // indirect
 	github.com/gofiber/fiber/v2 v2.25.0 // indirect
-	github.com/klauspost/compress v1.13.4 // indirect
+	github.com/gofiber/jwt/v3 v3.2.3 // indirect
+	github.com/golang-jwt/jwt/v4 v4.2.0 // indirect
+	github.com/klauspost/compress v1.14.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.32.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20210514084401-e8d321eab015 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 )
--- internal/http/handler/user.go
+++ internal/http/handler/user.go
@@ -4,8 +4,10 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/gomonitoring/http-server/internal/http/request"
 	"github.com/gomonitoring/http-server/internal/model"
 	"github.com/gomonitoring/http-server/internal/storage"
@@ -19,7 +21,7 @@ func (u User) SignUp(c *fiber.Ctx) error {
 	req := new(request.User)
 
 	if err := c.BodyParser(req); err != nil {
-		log.Printf("cannot user student data %s", err)
+		log.Printf("cannot load user data %s", err)
 
 		return fiber.ErrBadRequest
 	}
@@ -46,4 +48,46 @@ func (u User) SignUp(c *fiber.Ctx) error {
 	}
 
 	return c.Status(http.StatusCreated).JSON(user)
+}
+
+func (u User) Login(c *fiber.Ctx) error {
+	req := new(request.User)
+
+	if err := c.BodyParser(req); err != nil {
+		log.Printf("cannot load user data %s", err)
+
+		return fiber.ErrBadRequest
+	}
+
+	if err := req.Validate(); err != nil {
+		log.Printf("cannot validate user data %s", err)
+
+		return fiber.ErrBadRequest
+	}
+
+	user, err := u.Storage.LoadByUserPass(c.Context(), req.Username, req.Password)
+	if err != nil {
+		if errors.Is(err, storage.ErrorUserNotFound) {
+			return fiber.ErrNotFound
+		}
+
+		log.Printf("cannot load user %s", err)
+
+		return fiber.ErrInternalServerError
+	}
+
+	claims := jwt.MapClaims{
+		"username": user.Username,
+		"exp":      time.Now().Add(time.Hour * 72).Unix(), // read expire from configmap
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// read the secret from configmap
+	t, err := token.SignedString([]byte("secret"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.Status(http.StatusOK).JSON(fiber.Map{"token": t})
 }
