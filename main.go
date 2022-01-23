c647294d915ac82c8bd71bbbf94e2946c28b9095
Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>
2022-01-24 00:54:21 +0330

    complete main

--- main.go
+++ main.go
@@ -4,12 +4,23 @@ import (
 	"log"
 
 	jwtware "github.com/gofiber/jwt/v3"
+	"github.com/gomonitoring/http-server/internal/database"
+	"github.com/gomonitoring/http-server/internal/http/handler"
+	"github.com/gomonitoring/http-server/internal/storage"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	// create the db and pass it to the handler
+	db, err := database.NewDB()
+	if err != nil {
+		log.Fatalf("database connection failed %s", err)
+	}
+
+	hu := handler.User{
+		Storage: storage.NewPostgresDBStorage(db),
+	}
 
 	app := fiber.New()
 	// read secret from configmap
@@ -17,7 +28,7 @@ func main() {
 		SigningKey: []byte("secret"),
 	}))
 	userg := app.Group("/user")
-	hs.Register(userg)
+	hu.Register(userg)
 
 	// read port from configmap
 	if err := app.Listen(":8080"); err != nil {
