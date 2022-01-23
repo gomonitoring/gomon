8d9f8817f54f8f86bf101903df7cab81a9a97f83
Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>
2022-01-24 00:13:02 +0330

    add database

---
+++ internal/database/db.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gomonitoring/http-server/internal/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB() (*gorm.DB, error) {
+	// Connection URL to connect to Postgres Database, read from configmap
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	fmt.Println("Connection Opened to Database")
+	db.AutoMigrate(&model.User{})
+	fmt.Println("Database Migrated")
+
+	return db, nil
+}
