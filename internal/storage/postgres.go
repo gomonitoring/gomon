76fb637734abdd9c6fa685f833c164b099c58539
Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>
2022-01-24 14:13:05 +0330

    better errors

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/gomonitoring/http-server/internal/model"
 	"gorm.io/gorm"
@@ -21,7 +20,7 @@ func NewPostgresDBStorage(db *gorm.DB) PostgresDB {
 func (p PostgresDB) Save(ctx context.Context, u model.User) error {
 	err := p.db.Create(&u).Error
 	if err != nil {
-		return fmt.Errorf("postgres insert failed %w", err)
+		return ErrorUserDuplicate
 	}
 
 	return nil
@@ -32,8 +31,8 @@ func (p PostgresDB) LoadByUserPass(ctx context.Context, username string, passwor
 
 	p.db.Find(&user, "username = ? AND password = ?", username, password)
 
-	if user.ID == "" {
-		return model.User{}, fmt.Errorf("reading from postgres failed")
+	if user.Username == "" {
+		return model.User{}, ErrorUserNotFound
 	}
 
 	return user, nil
