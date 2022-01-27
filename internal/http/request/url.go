20f4dc8558e44ca82d96659defa289501a1db9d8
Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>
2022-01-27 10:33:12 +0330

    fill url model and request

--- internal/http/request/url.go
+++ internal/http/request/url.go
@@ -1,4 +1,26 @@
 package request
 
+import (
+	"fmt"
+
+	"github.com/go-ozzo/ozzo-validation/is"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
 type Url struct {
+	Name      string `json:"name"`
+	Url       string `json:"url"`
+	Threshold int    `json:"threshold"`
+}
+
+func (u Url) Validate() error {
+	if err := validation.ValidateStruct(&u,
+		validation.Field(&u.Name, validation.Required, is.UTFLetterNumeric),
+		validation.Field(&u.Url, validation.Required, is.URL),
+		validation.Field(&u.Threshold, validation.Required, is.Digit),
+	); err != nil {
+		return fmt.Errorf("url validation failed %w", err)
+	}
+
+	return nil
 }
--- internal/model/url.go
+++ internal/model/url.go
@@ -1,4 +1,15 @@
 package model
 
+import (
+	"gorm.io/gorm"
+)
+
 type Url struct {
+	gorm.Model
+	Name      string
+	Url       string
+	Threshold int
+	User      User
+	UserID    int
+	ResetTime int
 }
