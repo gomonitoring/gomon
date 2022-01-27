c29aed918467634be1cb1c4640162d4fe35e851f
mostafa <mos1380n@gmail.com>
2022-01-27 16:46:03 +0330

    Add task server getter

---
+++ internal/machinery/server/server.go
@@ -0,0 +1,24 @@
+package server
+
+import (
+	"github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/config"
+	settings "github.com/gomonitoring/http-server/internal/settings"
+)
+
+func GetMachineryServer() *machinery.Server {
+	taskserver, err := machinery.NewServer(&config.Config{
+		Broker:        settings.MachineryBroker,
+		ResultBackend: settings.MachineryResultBackend,
+		DefaultQueue:  "local",
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	taskserver.RegisterTasks(map[string]interface{}{
+		// TODO register tasks
+	})
+
+	return taskserver
+}
