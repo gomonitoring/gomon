9889376ec7184b8299997a97947c4970de5f28ae
mostafa <mos1380n@gmail.com>
2022-01-27 23:04:14 +0330

    Register tasks to server

--- internal/machinery/server/server.go
+++ internal/machinery/server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"github.com/RichardKnop/machinery/v1"
 	"github.com/RichardKnop/machinery/v1/config"
+	"github.com/gomonitoring/http-server/internal/machinery/tasks"
 	"github.com/gomonitoring/http-server/internal/settings"
 )
 
@@ -17,7 +18,10 @@ func GetMachineryServer() *machinery.Server {
 	}
 
 	taskserver.RegisterTasks(map[string]interface{}{
-		// TODO register tasks
+		"call_url":          tasks.CallUrl,
+		"collect_results":   tasks.CollectResults,
+		"create_alert":      tasks.CreateAlert,
+		"find_urls_to_call": tasks.FindUrlsToCall,
 	})
 
 	return taskserver
