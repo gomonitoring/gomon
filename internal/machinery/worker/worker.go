389a1773e6d001a2be9a9fe9cae6f15f26ac9972
mostafa <mos1380n@gmail.com>
2022-01-27 23:00:11 +0330

    Add periodic task to local worker

--- internal/machinery/worker/worker.go
+++ internal/machinery/worker/worker.go
@@ -2,23 +2,31 @@ package worker
 
 import (
 	"github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/tasks"
 	"github.com/gomonitoring/http-server/internal/settings"
 )
 
-func StartMonitoringWorker(taskserver *machinery.Server) error {
-
+func StartMonitoringWorker(taskserver *machinery.Server) {
 	worker := taskserver.NewCustomQueueWorker("monitoring_worker", settings.MonitoringWorkerConcurrency, "monitoring")
 	if err := worker.Launch(); err != nil {
 		panic(err.Error())
 	}
-	return nil
 }
 
-func StartLocalWorker(taskserver *machinery.Server) error {
-
+func StartLocalWorker(taskserver *machinery.Server) {
+	if err := registerPeriodicTasks(taskserver); err != nil {
+		panic(err.Error())
+	}
 	worker := taskserver.NewCustomQueueWorker("local_worker", settings.LocalWorkerConcurrency, "local")
 	if err := worker.Launch(); err != nil {
 		panic(err.Error())
 	}
-	return nil
+}
+
+func registerPeriodicTasks(taskserver *machinery.Server) error {
+	signature := &tasks.Signature{
+		Name: "find_urls_to_call",
+	}
+	err := taskserver.RegisterPeriodicTask("* * * * *", "find_urls_to_call", signature)
+	return err
 }
