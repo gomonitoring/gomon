abdbce8491f22b7c692e6246d10c5f5a1c4e6d66
mostafa <mos1380n@gmail.com>
2022-01-28 12:25:42 +0330

    Implement FindUrlsToCall

--- internal/machinery/tasks/find-urls-to-call.go
+++ internal/machinery/tasks/find-urls-to-call.go
@@ -1,6 +1,53 @@
 package tasks
 
+import (
+	"log"
+
+	"github.com/RichardKnop/machinery/v1/tasks"
+	"github.com/gomonitoring/http-server/internal/database"
+	"github.com/gomonitoring/http-server/internal/model"
+)
+
 func FindUrlsToCall() error {
-	// TODO find urls to call
+	db, _ := database.NewDB()
+	var urls []model.Url
+	query := db.Find(&urls)
+	groupSigs := make([]*tasks.Signature, query.RowsAffected)
+	for i, url := range urls {
+		sig := tasks.Signature{
+			Name:       "call_url",
+			RoutingKey: "monitoring",
+			Args: []tasks.Arg{
+				{
+					Name:  "url",
+					Type:  "string",
+					Value: url.Url,
+				}, {
+					Name:  "id",
+					Type:  "uint",
+					Value: url.ID,
+				}, {
+					Name:  "threshhold",
+					Type:  "int",
+					Value: url.Threshold,
+				}, {
+					Name:  "resetTime",
+					Type:  "int64",
+					Value: url.ResetTime,
+				},
+			},
+		}
+		groupSigs[i] = &sig
+	}
+	collectorSig := tasks.Signature{
+		Name:       "collect_results",
+		RoutingKey: "local",
+	}
+	group, _ := tasks.NewGroup(groupSigs...)
+	chord, _ := tasks.NewChord(group, &collectorSig)
+	_, err := GetMachineryServer().SendChord(chord, 0)
+	if err != nil {
+		log.Fatal("Could not push tasks to queue.")
+	}
 	return nil
 }
