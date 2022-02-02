e693f988b666c5bfc0706c81b9494088853d7d81
mostafa <mos1380n@gmail.com>
2022-02-03 00:28:59 +0330

    UPdate docker-compose.yml and entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 echo "RUNNING APPLICATION ..."
-
-echo "WAITING FOR DATABASE ..."
-while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 2; done  # TODO: this pars should probably be removed or 
-                                                       #       monitoring_worker should have a different
-echo "CONNECTED DATABASE ..."                          #       entry point script
+if [ $FUNC != "monitoring_worker" ]
+then
+    echo "WAITING FOR DATABASE ..."
+    while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 2; done
+    echo "CONNECTED DATABASE ..."   
+fi
 go run ./main.go $FUNC
