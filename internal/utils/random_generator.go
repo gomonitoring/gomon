7400ad8c9e04aa189ef69a50aa5c45f2a265f71d
Danyal Macky <60876722+DanialDMQ@users.noreply.github.com>
2022-07-09 21:24:20 +0430

    Auto populate (#9)
    * Rename, respect convention
    
    * Add auto populate feature via populate compose
    
    Co-authored-by: Daniyal-Mackey <daniyal.mackeyabadi@divar.ir>

---
+++ internal/utils/random_generator.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"math/rand"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
